95a80eeed6ec5402605eb9efd08e93cfebdd30c4
伊藤忍 <shinobu@itoushinobunoMacBook-Pro.local>
2019-08-17 00:45:33 +0900

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def hello
-    render html: "hello, world"
-  end
-
   def goodboy 
     render html: "good boy"
   end
