87f86ed7f0b6abd726ce6019a3a0965cf8c51f43
伊藤忍 <shinobu@itoushinobunoMacBook-Pro.local>
2019-08-17 01:12:28 +0900

    # Please enter the commit message for your changes. Lines starting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def goodboy 
-    render html: "good boy"
+    render html: "good boy!!!!!!!!!!!!!!"
   end
 end
